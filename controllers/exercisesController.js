eefd625b8f9929698adb12a61a6f09cf1910dfc2
aluno240 <felipe_official@outlook.com>
2022-04-05 21:54:01 -0300

    put

--- controllers/exercisesController.js
+++ controllers/exercisesController.js
@@ -14,8 +14,15 @@ exports.getExercises = (req, res, next) =>{
 exports.postExercises = (req, res, next) =>{
     const setAnswer = req.body.setAnswer
     console.log('answer ' + setAnswer)
-
+    
     res.render('exercises/exercises',{
+        pageTitle: 'Exercises',
+        test: 'this is a test',
+        activityKey: 0,
+        activity: 'Complete with the right verb "to be".',
+        question: 'you __ my friend.',
+        answer: 'are',
+        setAnswer: '',
         result: setAnswer
        })  
     
